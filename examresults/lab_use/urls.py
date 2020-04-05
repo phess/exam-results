7b1d53c1ba79d199acc1c6824fbf828f26dc28b2
Pablo N. Hess <phess@redhat.com>
2020-04-05 16:35:00 -0400

    Using generic views.

--- examresults/lab_use/urls.py
+++ examresults/lab_use/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'lab_use'
 urlpatterns = [
-        path('', views.new_result, name='tralala'),
+        #path('', views.new_result, name='add_result'),
+        path('', views.IndexView.as_view(), name='index'),
         path('report/', views.report),
+        path('new_result/', views.new_result, name='add_result'),
 ]
--- examresults/lab_use/views.py
+++ examresults/lab_use/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ExamResult, Institution
+from django.views import generic
 from django.template import loader
 
+class IndexView(generic.ListView):
+    template_name = 'lab_use/latest_results.html'
+    context_object_name = 'result_list'
+
+    def get_queryset(self):
+        """Return the last 10 submitted results."""
+        return ExamResult.objects.order_by('-exam_date')[:10]
+
 def new_result(request):
-    return HttpResponse('running new_result')
+    return render(request, 'lab_use/new_result.html')
+    #return HttpResponse('running new_result')
 
 def report(request):
     all_results = ExamResult.objects.order_by('-exam_date')
