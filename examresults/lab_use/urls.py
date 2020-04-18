2dd3cd0dac6b529485deaffe5aa2be5b4702c649
Pablo N. Hess <phess@redhat.com>
2020-04-18 19:30:47 -0400

    URLs reflect the workflow

--- examresults/lab_use/urls.py
+++ examresults/lab_use/urls.py
@@ -8,11 +8,19 @@ from . import views
 app_name = 'lab_use'
 urlpatterns = [
     path('', views.home_view),
-    path('home/', views.home_view),
+    #path('home/', views.home_view),
     path('report/', views.report_view),
-    path('new_result/', views.result_view, name='add_result'),
-    path('upload/', views.upload_view),
-    path('resend/', views.resend_view),
+    #path('new_result/', views.result_view, name='add_result'),
+    #path('upload/', views.upload_view),
+    #path('resend/', views.resend_view),
     path('pdf_generate/', views.pdf_generate_view),
     path('pdf_generate/<int:id>/', views.pdf_generate_view),
+    
+    path('start/', views.start),
+    path('extraction/start/', views.start_extraction),
+    path('extraction/end/', views.end_extraction),
+    path('extraction/view/<int:id>/', views.view_extraction),
+    path('pcr/start/', views.start_pcr),
+    path('pcr/view/<int:id>/', views.view_pcr),
+    path('pcr/end/', views.end_pcr),
 ]
